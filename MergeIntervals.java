124e468256a0839391cc658c8c89df8c71f51aab
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2022-07-26 23:31:54 +0530

    Merge Intervals

---
+++ MergeIntervals.java
@@ -0,0 +1,52 @@
+// Question
+
+// 56. Merge Intervals
+
+// Medium
+
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+// Example 2:
+
+// Input: intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+// Constraints:
+
+// 1 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 104
+
+// ************************************************************************************************
+// Solution
+class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
+        
+        ArrayList<int[]> list = new ArrayList<>();
+        
+        for(int[] interval : intervals){
+            if(list.size() == 0){
+                list.add(interval);
+            }else{
+                int prevInterval[] = list.get(list.size() - 1);                
+                if(interval[0] <= prevInterval[1]){
+                    prevInterval[1] = Math.max(interval[1],prevInterval[1]);
+                }else{
+                    list.add(interval);
+                }
+            }
+            
+            
+        }
+                
+        return list.toArray(new int[list.size()][]);
+    }
+}
